fb34bdb3b65df263efb33e6f1c85d5ec3c516384
muzaffarhaque <muzaffarhaque777@gmail.com>
2024-04-14 16:22:23 +0530

    dubject : calculater completed

---
+++ script.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    $('#taxForm').submit(function(event) {
+      event.preventDefault();
+      calculateTax();
+    });
+  
+    $('#closeBtn').click(function() {
+      $('#resultModal').hide();
+    });
+  });
+  
+  function calculateTax() {
+    var grossIncome = parseFloat($('#grossIncome').val());
+    var extraIncome = parseFloat($('#extraIncome').val());
+    var ageGroup = $('#ageGroup').val();
+    var deductions = parseFloat($('#deductions').val());
+    var totalIncome = grossIncome + extraIncome - deductions;
+    var tax = 0;
+  
+    if (totalIncome > 800000) {
+      var taxableAmount = totalIncome - 800000;
+      if (ageGroup === '<40') {
+        tax = taxableAmount * 0.3;
+      } else if (ageGroup === '>=40&<60') {
+        tax = taxableAmount * 0.4;
+      } else if (ageGroup === '>=60') {
+        tax = taxableAmount * 0.1;
+      }
+    }
+  
+    $('#result').text(totalIncome - tax);
+    $('#resultModal').show();
+  }
+  
